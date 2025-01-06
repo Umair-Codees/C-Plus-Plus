7cffa9ae5fe2c26e33aa8c3f552a3f9e286ae36f
Umair Ali <workumair20@gmail.com>
2025-01-06 17:47:29 +0500

    gcd.cpp

--- Recursion/gcd.cpp
+++ Recursion/gcd.cpp
@@ -1,14 +1,14 @@
-//#include <iostream>
-//using namespace std;
-//
-//int gcd(int a, int b) {
-//	if (b == 0) {
-//		return a;
-//	}
-//	return gcd(b, a % b);
-//}
-//
-//int main() {
-//	cout << gcd(18, 48);
-//	return 0;
-//}
+#include <iostream>
+using namespace std;
+
+int gcd(int a, int b) {
+	if (b == 0) {
+		return a;
+	}
+	return gcd(b, a % b);
+}
+
+int main() {
+	cout << gcd(18, 48);
+	return 0;
+}
